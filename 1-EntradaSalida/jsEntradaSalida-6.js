875dbf583ccaf63fcdb2c92543d5a5dc086b9a98
cristianfabiocelano <cristianfabiocelano@gmail.com>
2017-06-26 22:52:56 -0300

    Revert "Revert "terminado 26/06""
    This reverts commit af6160ffc65179039de8ac7c19815bf58e310b7f.

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var a = document.getElementById("numeroUno").value;
 
+    var b = document.getElementById("numeroDos").value;
+
+    var perro = parseInt (a);
+
+    var gato = parseInt (b);
+    
+   alert ("El resultado es " + (perro + gato) );
 }
+
 
